3cc6e160686ca1786f88dbd5d59e90bcf8272507
Maksim Sidorov <maxim_sdrv@mail.ru>
2025-07-01 13:07:29 +0300

    * feat(api): add websocket handlers, ping-pong

--- newService/proxy/internal/services/target_provider/service.go
+++ newService/proxy/internal/services/target_provider/service.go
@@ -1,10 +1,13 @@
 package target_provider
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
+	"github.com/coder/websocket"
 	"github.com/opticoder/ctx-log/go/ctx_log"
 	"github.com/teivah/broadcast"
 
@@ -21,8 +24,13 @@ type Connection struct {
 }
 
 const (
-	msgTypeSensorInfo = "sensor_info"
-	msgTypeJammerInfo = "jammer_info"
+	msgTypeSensorInfo    = "sensor_info"
+	msgTypeLicenseStatus = "license_status"
+)
+
+const (
+	fieldMessage = "message"
+	fieldData    = "data"
 )
 
 type Device struct {
@@ -32,7 +40,6 @@ type Device struct {
 	host            string
 	urlWs           string
 	sensorInfoRelay *broadcast.Relay[*dss_target_service.SensorInfo]
-	jammerInfoRelay *broadcast.Relay[*dss_target_service.JammerInfo]
 	targetRelay     *broadcast.Relay[*dss_target_service.TargetData]
 }
 
@@ -58,7 +65,6 @@ func New(ctx context.Context, connections []Connection) *Service {
 			target:          apiClient.TargetAPI,
 			host:            connection.Host,
 			sensorInfoRelay: broadcast.NewRelay[*dss_target_service.SensorInfo](),
-			jammerInfoRelay: broadcast.NewRelay[*dss_target_service.JammerInfo](),
 			targetRelay:     broadcast.NewRelay[*dss_target_service.TargetData](),
 		}
 
@@ -128,6 +134,106 @@ func (s *Device) connect(ctx context.Context, portWs int) {
 		break
 	}
 
+	err := s.GetSettings(ctx)
+	if err != nil {
+		logger.WithError(err).Error("Failed to get settings")
+	}
+
 	s.urlWs = fmt.Sprintf("ws://%s:%d", s.host, portWs)
 	s.startConnection(ctx)
+}
+
+func (s *Device) updateLoop(ctx context.Context, conn *websocket.Conn) {
+	logger := logging.WithCtxFields(ctx)
+
+	for {
+		_, data, err := conn.Read(ctx)
+		if err != nil {
+			logger.WithError(err).Warn("Failed to read data from DDS target service")
+			break
+		}
+
+		data = bytes.ReplaceAll(
+			data,
+			[]byte(`"jammer_mode":"UNDEFINED"`),
+			[]byte(`"jammer_mode":"AUTO"`),
+		)
+
+		var fields map[string]json.RawMessage
+		if err = json.Unmarshal(data, &fields); err != nil {
+			logger.WithError(err).Error("Unmarshalling message failed")
+			continue
+		}
+
+		msgTypeRaw, ok := fields[fieldMessage]
+		if !ok {
+			logger.Errorf("Missing message field %s", fieldMessage)
+			continue
+		}
+		var msgType string
+		if err = json.Unmarshal(msgTypeRaw, &msgType); err != nil {
+			logger.WithError(err).Error("Unmarshalling message failed")
+			continue
+		}
+
+		dataRaw, ok := fields[fieldData]
+		if !ok {
+			logger.Errorf("Missing message field %s", fieldData)
+			continue
+		}
+
+		if err = s.processMessage(ctx, msgType, dataRaw); err != nil {
+			logger.WithError(err).Errorf("Failed to process message type: %s", msgType)
+			continue
+		}
+
+		logger.Tracef("Successfully processed message type: %s", msgType)
+	}
+	logger.Warn("UpdateLoop ended")
+}
+
+func (s *Device) processMessage(ctx context.Context, msgType string, dataRaw json.RawMessage) error {
+	logger := logging.WithCtxFields(ctx)
+
+	switch msgType {
+	case msgTypeSensorInfo:
+		return s.processSensorInfo(ctx, dataRaw)
+	case msgTypeLicenseStatus:
+		return s.processLicenseStatus(ctx, dataRaw)
+	default:
+		logger.Tracef("Unknown message type: %s", msgType)
+		return nil
+	}
+}
+
+func (s *Device) processSensorInfo(ctx context.Context, dataRaw json.RawMessage) error {
+	logger := logging.WithCtxFields(ctx)
+
+	var sensorInfo dss_target_service.SensorInfo
+
+	if err := json.Unmarshal(dataRaw, &sensorInfo); err != nil {
+		logger.WithError(err).Error("Unmarshalling message failed")
+		return err
+	}
+
+	logger.Debugf("Sensor info: %+v", sensorInfo)
+
+	s.sensorInfoRelay.Broadcast(&sensorInfo)
+
+	return nil
+}
+
+func (s *Device) processLicenseStatus(ctx context.Context, dataRaw json.RawMessage) error {
+	logger := logging.WithCtxFields(ctx)
+
+	var licenseStatus dss_target_service.LicenseStatus
+
+	if err := json.Unmarshal(dataRaw, &licenseStatus); err != nil {
+		logger.WithError(err).Error("Unmarshalling message failed")
+		return err
+	}
+
+	logger.Debugf("License status is: %v %s", licenseStatus.Valid, licenseStatus.Description)
+
+	return nil
 }
