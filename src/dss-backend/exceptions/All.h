8a0c0829688b7cd62ceea082f37b3204a54bf78a
Maksim Sidorov <maxim_sdrv@mail.ru>
2025-07-08 00:03:29 +0300

    * feat: remove src

--- src/dss-backend/exceptions/All.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "dss-backend/exceptions/DuplicateParserKey.h"
-#include "dss-backend/exceptions/InvalidConfig.h"
-#include "dss-backend/exceptions/InvalidConfigValue.h"
-#include "dss-backend/exceptions/MissingParser.h"
--- src/dss-common/config/HttpTags.h
+++
@@ -1,8 +0,0 @@
-namespace mpk::dss::config
-{
-
-constexpr auto httpTag = "http";
-constexpr auto wsTag = "websocket";
-constexpr auto authTag = "auth";
-
-} // namespace mpk::dss::config
