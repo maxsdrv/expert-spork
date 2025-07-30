69f61f341560c0915bf517742db1a15451f504b9
Maksim Sidorov <maxim_sdrv@mail.ru>
2025-07-30 18:05:48 +0300

    * feat(api): add some functional

--- target-service/migrations/apply.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-atlas migrate apply \
-  --dir "file://migrations" \
-  --url postgres://$DB_PG_USER:$DB_PG_PASS@$DB_PG_HOST:$DB_PG_PORT/$DB_PG_NAME?sslmode=disable
--- target-service/tests/api/integration/helpers/misc.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import uuid
-
-
-def sleep(delay):
-    return asyncio.sleep(delay)
-
-def gen_uuid():
-    return uuid.uuid4().hex
