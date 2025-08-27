b8ab9512d2b39d52999894fe928106a30f33c684
maxsdrv <maxim_sdrv@mail.ru>
2025-08-27 17:21:20 +0300

    * fix(api): update service

--- irz_service/internal/core/geo_coordinate.go
+++
@@ -1,9 +0,0 @@
-package core
-
-type AngleType float32
-type CoordinateType float64
-
-type GeoCoordinate struct {
-	lon CoordinateType
-	lat CoordinateType
-}
--- new-service/configs/set_profiles_defaults.sh
+++
@@ -1,4 +0,0 @@
-if is_in bulat "$DEVSPACE_PROFILES"; then
-  export BULAT_DEVICES="127.0.0.1 $BULAT_DEVICES"
-fi
-
